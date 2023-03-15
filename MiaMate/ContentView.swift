6c68e3a3690c4a9439d162756ef5f1c69b53ff10
Mendy <mendydnem@hotmail.com>
2023-03-15 03:19:51 +0100

    automatic scrollview

--- MiaMate/ContentView.swift
+++ MiaMate/ContentView.swift
@@ -2,6 +2,13 @@ import SwiftUI
 import OpenAISwift
 import AVFoundation
 
+struct ViewHeightKey: PreferenceKey {
+    static var defaultValue: CGFloat { 0 }
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = max(value, nextValue())
+    }
+}
+
 struct ContentView: View {
 
     @ObservedObject var viewModel = ViewModel()
@@ -22,9 +29,8 @@ struct ContentView: View {
     let prompt = ChatMessage(role: .assistant, content: "Hi! My name is Mia, a virtual health coach who has knowledge about Chronic Fatigue Syndrome. How are you today?")
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .center) {
             HStack {
-                Spacer()
                 VStack {
                     Text("MiaMate")
                         .font(.title)
@@ -32,56 +38,87 @@ struct ContentView: View {
                     
                     Image("Avatar")
                         .resizable()
+                        .padding(.bottom)
                         .frame(width: 300, height: 300)
+                    
+                    // speaker
+                    Button(action: {
+                        self.isSpeakingEnabled.toggle()
+                    }, label: {
+                        Image(systemName: "speaker.wave.2.fill")
+                            .font(.title)
+                            .foregroundColor(isSpeakingEnabled ? .blue : .gray)
+                    })
 
                 }
-                Spacer()
             }
-            Spacer()
 
-            
             ScrollViewReader { scrollview in
                 ScrollView(.vertical) {
-                    VStack(alignment: .leading, spacing: 10) {
-                        ForEach(Array(messages.enumerated()), id: \.offset) { index, message in
-                            // do not show systemMessage
+                    ForEach(Array(messages.enumerated()), id: \.offset) { index, message in
+                        HStack {
                             if message.role != .system {
-                                Text(message.content).bold(message.role == .user)
+                                Text(message.content)
+                                    .bold(message.role == .user)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0))
                             }
-                            
                         }
                     }
-                        
+                    .background(
+                        GeometryReader { geometry in
+                            Color.clear.preference(
+                                key: ViewHeightKey.self,
+                                value: geometry.size.height
+                            )
+                        }
+                    )
                     
-                }.padding(.all, 20.0)
-                 .frame(maxHeight: 350)
-                 .background(RoundedRectangle(cornerRadius: 20)
-                 .stroke(Color.gray, lineWidth: 1))
-            }
-
-            Spacer()
-            
-            HStack {
-                TextField("Start chatting...", text: $text)
-                    .padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 0))
-                    .background(RoundedRectangle(cornerRadius: 50)
-                        .stroke(Color.gray, lineWidth: 1))
-
-                Button("Send") {
-                    Task.detached {
-                        await send()
+                    .onPreferenceChange(ViewHeightKey.self) { height in
+                        if height > 0 {
+                            withAnimation {
+                                scrollview.scrollTo(messages.count - 1, anchor: .bottom)
+                            }
+                        }
                     }
                 }
+                .padding(.all, 20.0)
+                .frame(height: 200.0)
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .overlay(
+                    Spacer()
+                )
+            }.padding(.top, 20.0)
+            
+            Spacer()
+            
+            VStack {
                 
-                // speaker
-                Button(action: {
-                    self.isSpeakingEnabled.toggle()
-                }, label: {
-                    Image(systemName: "speaker.wave.2.fill")
-                        .font(.title)
-                        .foregroundColor(isSpeakingEnabled ? .blue : .gray)
-                })
+                HStack {
+                    TextField("Start chatting...", text: $text)
+                        .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 5))
+                    
+                    Button(action: {
+                        Task.detached {
+                            await send()
+                        }
+                    }, label: {
+                        Image(systemName: "paperplane.fill")
+                            .padding(.trailing, 20.0)
+                    })
+                }
+                .background(
+                    RoundedRectangle(cornerRadius: 20).fill(Color(hue: 1.0, saturation: 0.0, brightness: 0.982))
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.gray, lineWidth: 0.5)
+                )
             }
+            .frame(height: 0.0)
         }
         .padding()
         .task {
@@ -101,9 +138,9 @@ struct ContentView: View {
         // create new chat message with user's text
         let userMessage = ChatMessage(role: .user, content: text)
         
-        // append user's message to messages array
-        messages.append(userMessage)
 
+        self.messages.append(userMessage)
+        
         // send chat convo to API and receive response
         await viewModel.send(chat: messages) { response in
             
@@ -111,7 +148,7 @@ struct ContentView: View {
             
             DispatchQueue.main.async {
                 // async append API's response to [messages]
-                self.messages.append(miaMessage)
+                    self.messages.append(miaMessage)
                 
                 // if speaker is on
                 if self.isSpeakingEnabled {
