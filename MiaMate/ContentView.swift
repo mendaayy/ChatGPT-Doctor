1969b5a4e28f8208d5fc80b211377782c954735c
Mendy <mendydnem@hotmail.com>
2023-03-15 11:35:04 +0100

    updated ui

--- MiaMate/ContentView.swift
+++ MiaMate/ContentView.swift
@@ -29,108 +29,119 @@ struct ContentView: View {
     let prompt = ChatMessage(role: .assistant, content: "Hi! My name is Mia, a virtual health coach who has knowledge about Chronic Fatigue Syndrome. How are you today?")
     
     var body: some View {
-        VStack(alignment: .center) {
-            HStack {
-                VStack {
-                    Text("MiaMate")
-                        .font(.title)
-                        .bold()
-                    
-                    Image("Avatar")
-                        .resizable()
-                        .padding(.bottom)
-                        .frame(width: 300, height: 300)
-                    
-                    // speaker
-                    Button(action: {
-                        self.isSpeakingEnabled.toggle()
-                    }, label: {
-                        Image(systemName: "speaker.wave.2.fill")
+        ZStack {
+            Image("Background")
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+                .frame(minWidth: 0, maxWidth: .infinity)
+            
+            VStack(alignment: .center) {
+                HStack {
+                    VStack {
+                        Text("MiaMate")
                             .font(.title)
-                            .foregroundColor(isSpeakingEnabled ? .blue : .gray)
-                    })
-
+                            .foregroundColor(Color(hue: 0.712, saturation: 0.608, brightness: 0.537))
+                            .bold()
+            
+                        Image("Avatar")
+                            .resizable()
+                            .padding(.bottom)
+                            .frame(width: 270.0, height: 270.0)
+                        
+                        // speaker
+                        Button(action: {
+                            self.isSpeakingEnabled.toggle()
+                        }, label: {
+                            Image(systemName: "speaker.wave.2.fill")
+                                .font(.title)
+                                .foregroundColor(isSpeakingEnabled ? .blue : .gray)
+                        })
+                        
+                    }
                 }
-            }
-
-            ScrollViewReader { scrollview in
-                ScrollView(.vertical) {
-                    ForEach(Array(messages.enumerated()), id: \.offset) { index, message in
-                        HStack {
-                            if message.role != .system {
-                                Text(message.content)
-                                    .bold(message.role == .user)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0))
+                
+                ScrollViewReader { scrollview in
+                    ScrollView(.vertical) {
+                        ForEach(Array(messages.enumerated()), id: \.offset) { index, message in
+                            HStack {
+                                if message.role != .system {
+                                    Text(message.content)
+                                        .bold(message.role == .user)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0))
+                                }
+                            }
+                        }
+                        .background(
+                            GeometryReader { geometry in
+                                Color.clear.preference(
+                                    key: ViewHeightKey.self,
+                                    value: geometry.size.height
+                                )
+                            }
+                        )
+                        
+                        .onPreferenceChange(ViewHeightKey.self) { height in
+                            if height > 0 {
+                                withAnimation {
+                                    scrollview.scrollTo(messages.count - 1, anchor: .bottom)
+                                }
                             }
                         }
                     }
-                    .background(
-                        GeometryReader { geometry in
-                            Color.clear.preference(
-                                key: ViewHeightKey.self,
-                                value: geometry.size.height
-                            )
-                        }
+                    .padding(.all, 20.0)
+                    .frame(height: 200.0)
+                    
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+                    .background(RoundedRectangle(cornerRadius: 20).fill(Color(hue: 0.0, saturation: 0.007, brightness: 0.941)))
+                    .overlay(
+                       Spacer()
                     )
                     
-                    .onPreferenceChange(ViewHeightKey.self) { height in
-                        if height > 0 {
-                            withAnimation {
-                                scrollview.scrollTo(messages.count - 1, anchor: .bottom)
-                            }
-                        }
-                    }
-                }
-                .padding(.all, 20.0)
-                .frame(height: 200.0)
-                .background(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(Color.gray, lineWidth: 1)
-                )
-                .overlay(
-                    Spacer()
-                )
-            }.padding(.top, 20.0)
-            
-            Spacer()
-            
-            VStack {
+                }.padding(.top, 20.0)
                 
-                HStack {
-                    TextField("Start chatting...", text: $text)
-                        .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 5))
+                Spacer()
+                    .frame(height: 100.0)
+                
+                VStack {
                     
-                    Button(action: {
-                        Task.detached {
-                            await send()
-                        }
-                    }, label: {
-                        Image(systemName: "paperplane.fill")
-                            .padding(.trailing, 20.0)
-                    })
+                    HStack {
+                        TextField("Start chatting...", text: $text)
+                            .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 5))
+                        
+                        Button(action: {
+                            Task.detached {
+                                await send()
+                            }
+                        }, label: {
+                            Image(systemName: "paperplane.fill")
+                                .padding(.trailing, 20.0)
+                        })
+                    }
+                    .background(
+                        RoundedRectangle(cornerRadius: 20).fill(Color(hue: 1.0, saturation: 0.0, brightness: 0.982))
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.gray, lineWidth: 0.5)
+                    )
                 }
-                .background(
-                    RoundedRectangle(cornerRadius: 20).fill(Color(hue: 1.0, saturation: 0.0, brightness: 0.982))
-                )
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(Color.gray, lineWidth: 0.5)
-                )
             }
-            .frame(height: 0.0)
-        }
-        .padding()
-        .task {
-            viewModel.initialize()
-            
-            // add prompt message when app launches
-            messages.append(initPrompt)
-            messages.append(prompt)
-            
+            .padding(20.0)
+            .task {
+                viewModel.initialize()
+                
+                // add prompt message when app launches
+                messages.append(initPrompt)
+                messages.append(prompt)
+                
+            }
         }
     }
-
+    
     func send() async {
         // if text field empty, then return
         guard !text.trimmingCharacters(in: .whitespaces).isEmpty else { return }
@@ -170,7 +181,6 @@ struct ContentView: View {
         // Speak the utterance
         synthesizer.speak(speechUtterance)
     }
-
 }
 
 struct ContentView_Previews: PreviewProvider {
